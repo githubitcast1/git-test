bf5cc7d6b30096661943b3706147819dce64950f
zhangsan <zhangsan@itcast.cn>
2018-12-22 19:57:00 +0800

    修改firstclass

--- maven_parent/maven_dao/src/main/java/com.itheima/Test.java
+++ maven_parent/maven_dao/src/main/java/com.itheima/Test.java
@@ -2,6 +2,10 @@ package com.itheima;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(122343);
+
+        System.out.println(122343+"修改");
+
     }
+
+
 }
